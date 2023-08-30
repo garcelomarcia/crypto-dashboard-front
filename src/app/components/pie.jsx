0649dbb471dce5da42a0dd2455a154216a4a3092
garcelomarcia <marcelogarcia91@gmail.com>
2023-08-30 12:10:21 -0600

    changed requests to server route

--- src/app/components/pie.jsx
+++ src/app/components/pie.jsx
@@ -20,43 +20,27 @@ const PieChart = ({ category }) => {
   const [colors, setColors] = useState([]);
 
   const getCategoryData = async () => {
-    if (category === "") {
-      const response = await axios.get(
-        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en`
-      );
+    const apiUrl =
+      category === ""
+        ? "https://fast-delivery-server.xyz/api/"
+        : `https://fast-delivery-server.xyz/api/${category}`;
+
+    try {
+      const response = await axios.get(apiUrl);
       setCategoryDetail(response.data.slice(0, 10));
-    } else {
-      const response = await axios.get(
-        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&category=${category}&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en`
-      );
-      setCategoryDetail(response.data.slice(0, 10));
+      getColors();
+    } catch (error) {
+      console.error("Error fetching data:", error);
     }
-    getColors();
   };
 
   const getColors = async () => {
-    const colorThief = new ColorThief();
-    const colorsArray = [];
-    const imageUrls = categoryDetail.map((coin) => coin.image);
+    const imageUrls = req.body.map((coin) => coin.image);
+    const response = await axios.post("https://fast-delivery-server/api/", {
+      body: imageUrls,
+    });
 
-    for (const imageUrl of imageUrls) {
-      const image = new Image();
-      image.crossOrigin = "Anonymous";
-      image.src = imageUrl;
-
-      const dominantColor = await new Promise((resolve) => {
-        image.onload = () => {
-          resolve(colorThief.getColor(image));
-        };
-      });
-
-      // Convert RGB array to a string representation
-      const rgbString = `rgb(${dominantColor[0]}, ${dominantColor[1]}, ${dominantColor[2]})`;
-
-      colorsArray.push(rgbString);
-    }
-
-    setColors(colorsArray);
+    setColors(response.data);
   };
 
   useEffect(() => {
