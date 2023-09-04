58728afa206b3841c853c418a456d1d2a211f7d4
garcelomarcia <marcelogarcia91@gmail.com>
2023-09-04 13:13:26 -0600

    sound and toast alerts are working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import io from "socket.io-client";
 import { Order, Liquidation } from "./interfaces";
 import Table from "./components/table";
@@ -8,23 +8,21 @@ import TradingViewWidget from "./components/tradingview";
 import HorizontalBarChart from "./components/categories";
 import PieChart from "./components/pie";
 import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { useCustomSound } from "./utils/useCustomSound";
 import debounce from "lodash/debounce";
 
 const socket = io("https://fast-delivery-server.xyz/");
 
 export default function Home() {
+  const { playMarimbaSound, playLiquidationSound } = useCustomSound();
+
   const [orders, setOrders] = useState<Order[]>([]);
   const [alertOrders, setAlertOrders] = useState<Order[]>([]);
   const [liquidations, setLiquidations] = useState<Liquidation[]>([]);
   const [selectedCategory, setSelectedCategory] = useState<string>("");
   const [pair, setPair] = useState("BTCUSDT");
-  const { playMarimbaSound, playLiquidationSound } = useCustomSound();
-
-  const debouncedPlaySound = debounce(
-    () => playMarimbaSound(),
-    3600000
-  ) as () => void;
+  const [isLoading, setIsLoading] = useState(true);
 
   const handleRowClick = (selectedPair: string) => {
     setPair(selectedPair);
@@ -34,19 +32,49 @@ export default function Home() {
     setSelectedCategory(category);
   };
 
-  const handleOrderAlerts = () => {
-    // Check if any order has a distance less than 0.5 and play the sound
-    const isAlertTriggered = orders.some(
-      (order: Order) => order.distance < 0.5
-    );
+  const firstButtonRef = useRef<HTMLButtonElement | null>(null);
 
-    if (isAlertTriggered) {
-      debouncedPlaySound(); // Play the sound, but only once per hour
+  const clickFirstButton = () => {
+    if (firstButtonRef.current) {
+      firstButtonRef.current.click();
+    }
+  };
+
+  function getArrayDifference(array1: Order[], array2: Order[]) {
+    const compareObjects = (obj1: Order, obj2: Order) => {
+      return obj1.id === obj2.id;
+    };
+
+    const difference = array1.filter((item1) => {
+      return !array2.some((item2) => compareObjects(item1, item2));
+    });
+
+    return difference;
+  }
+
+  const handleOrderAlerts = () => {
+    const ordersClose = orders.filter((order: Order) => order.distance < 0.5);
+    const difference = getArrayDifference(ordersClose, alertOrders);
+    console.log("ORDERS CLOSE", ordersClose);
+    console.log("ALERT ORDERS", alertOrders);
+    console.log("DIFFERENCE", difference);
+    if (difference.length > 0) {
+      clickFirstButton();
+      toast(`Big Order Alert ${difference[0].pair} @ ${difference[0].price}`, {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+      setAlertOrders(ordersClose);
     }
   };
 
   useEffect(() => {
-    socket.on("initialOrders", (data) => setAlertOrders(data));
     socket.on("databaseChange", (data) => {
       setOrders(data);
     });
@@ -56,19 +84,45 @@ export default function Home() {
   }, []);
 
   useEffect(() => {
-    handleOrderAlerts();
-  }, [orders]);
+    if (!isLoading) {
+      handleOrderAlerts();
+    }
+  }, [orders, isLoading]);
 
   useEffect(() => {
-    playLiquidationSound();
-    toast("New Liquidation!");
-  }, [liquidations]);
+    if (!isLoading) {
+      playLiquidationSound();
+      if (liquidations.length > 0) {
+        toast.error(`New Liquidation on symbol ${liquidations[0].symbol}`, {
+          position: "top-center",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+      }
+    }
+  }, [liquidations, isLoading]);
 
-  console.log(liquidations);
+  console.log(alertOrders);
 
   return (
     <div>
-      <ToastContainer />
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
       <div className="flex flex-col xl:flex-row px-4 xl:px-10">
         <div className="w-full xl:w-2/3 xl:pr-4 mb-4 xl:mb-0">
           <h1 className="font-sans text-lg">Chart</h1>
@@ -106,8 +160,10 @@ export default function Home() {
         </div>
       </div>
       <div>
-        <button onClick={playLiquidationSound}></button>
-        <button onClick={playMarimbaSound}></button>
+        <button onClick={playLiquidationSound}>.</button>
+        <button onClick={playMarimbaSound} ref={firstButtonRef}>
+          .
+        </button>
       </div>
     </div>
   );
